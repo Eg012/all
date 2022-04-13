c42024f145ee8c6e8c9267fb09a1442a882d11f7
Egor <savinovego05@gmail.com>
2022-04-13 19:48:07 +0300

    data_base

--- data_base.py
+++ data_base.py
@@ -14,54 +14,82 @@ class Base:
             with open("Data_Base.json", "w") as db_file:
                 json.dump({}, db_file)
 
-    def init_table(self, name):
-        if self.content.get(name) is None:
-            self.content[name] = []
+    def init_table(self, schema):
+        if self.content.get(schema.get('_table')) is None:
+            self.content[schema.get('_table')] = []
 
-    def create(self, table_name, cls, data):
-        all_data = cls
-        all_data.update(data)
-        print(all_data)
-        self.content[table_name].append(all_data)
+    def create(self, schema, data):
+        table_name = schema.get('_table')
+        cls = dict(schema)
+        del cls['_table']
+        cls.update(data)
+        print(cls)
+        self.content[table_name].append(cls)
 
     def save(self):
         with open("Data_Base.json", "w") as db_file:
             json.dump(self.content, db_file)
 
-    def remove(self):
-        # with open('Data_Base.json', 'r') as db_file:
-        #  self.content = json.read(db_file)
-        #
-        #  if db_file in db_file:
-        #      db_file.id == id
-        pass
+    def get(self, schema, filters=None):
+        table_name = schema.get('_table')
+        with open("Data_Base.json", "w") as db_file:
+            json.dump(self.content, db_file)
+        # self.content[table]
+        if filters is None:
+            return self.content[table_name]
 
+        contents = []
+        for item in self.content[table_name]:
+            for key in filters.keys():
+                if type(filters[key]) == list and item[key] in filters[key]:
+                    contents.append(item)
+                elif item[key] == filters[key]:
+                    contents.append(item)
+
+        return contents
+
+    def remove(self, schema, filters):
+        table_name = schema.get('_table')
+        with open('Data_Base.json', 'w') as db_file:
+            json.dump(self.content, db_file)
+
+        for item in self.content[table_name]:
+            for key in filters.keys():
+                if type(filters[key]) == list and item[key] in filters[key]:
+                    self.content[table_name].remove(item)
+                elif item[key] == filters[key]:
+                    self.content[table_name].remove(item)
 
 User = {
+    "_table": "users",
     "name": "",
     "password": ''
 }
 
 Token = {
+    "_table": "tokens",
     "id": "2",
 }
 
-
 db = Base()
 db.connect()
-db.init_table('users')
-db.init_table('tokens')
-db.create('users', User, {
+db.init_table(User)
+db.init_table(Token)
+db.create(User, {
     "name": "Ivan",
     "password": "123"
 })
-db.create('users', User, {
+db.create(User, {
     "name": "Ivan1",
     "password": "1232"
 })
 
-db.create('tokens', Token, {
+db.create(Token, {
     "id": "10"
 })
 
+db.save()
+db.remove(User, {
+                "name": "Ivan1",
+})
 db.save()
